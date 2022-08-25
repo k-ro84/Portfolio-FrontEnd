748be76fcc70a0343ccd181560dff55001d609f5
Carolina Olguin <k-rito469@hotmail.com>
2022-08-24 23:01:22 -0300

    cuarto commit con angular

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,7 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  correo: string = '';
+  password: string = '';
+  
 
+  cambiaCorreo(string:any) {
+    // Podes consultar el valor por consola
+    console.log(string);
+    this.correo = string;
+  }
+
+  cambiaPassword(string:any) {
+    // Podes consultar el valor por consola
+    console.log(string);
+    this.password = string;
+  }
+
+  onClick() {
+    alert('Hiciste click!!');
+  }
   constructor() { }
 
   ngOnInit(): void {
