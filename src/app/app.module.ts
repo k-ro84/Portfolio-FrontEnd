12b0fc78d18e0b4adb0ba369661e93d595fd7596
Carolina Olguin <k-rito469@hotmail.com>
2022-08-28 20:50:59 -0300

    6 de angular/form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -39,6 +39,7 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     NgCircleProgressModule.forRoot({}),
     
   ],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,19 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup} from '@angular/forms';
+import {Validators} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  correo: string = '';
-  password: string = '';
+  title :string= 'Registro de usuario';
+
+  
+  form: FormGroup;
+
+  // Inyectar en el constructor el formBuilder
+  constructor(private formBuilder: FormBuilder){ 
+   
+    this.form= this.formBuilder.group({
+      password:['',[Validators.required, Validators.minLength(8),Validators.maxLength(15)]],
+      email:['', [Validators.required, Validators.email]],
+   })
+  }
   
 
-
-  constructor() { }
-
+ 
   ngOnInit(): void {
   }
+
+  get Password(){
+    return this.form.get("password");
+  }
+ 
+  get Mail(){
+   return this.form.get("email");
+  }
+
+  get PasswordValid(){
+    return this.Password?.touched && !this.Password?.valid;
+  }
+
+  get MailValid() {
+    return false
+  }
+ 
+
+  onEnviar(event: Event){
+   
+    event.preventDefault; 
+ 
+    if (this.form.valid){
+     
+      alert("Todo salio bien ¡Enviar formuario!")
+    }else{
+      
+      this.form.markAllAsTouched(); 
+    }
+   }
 
 }
